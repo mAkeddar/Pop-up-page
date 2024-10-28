0e003ddfc127aea8d12441ab19c254a78fd9028d
mAkeddar <mehdi.akeddar@gmail.com>
2024-10-28 15:40:19 +0100

    2 test

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import qrcode
 
 # URL of the hosted HTML page
-page_url = "https://yourdomain.com/location_page.html"
+page_url = "https://makeddar.github.io/Pop-up-page/"
 
 # Generate the QR code
 qr = qrcode.make(page_url)
